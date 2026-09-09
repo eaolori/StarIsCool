8e76a1af79107708728936971f6f45d69d826a99
ea.olori <ea.olori@gmail.com>
2026-09-09 14:18:35 +0100

    Day2

---
+++ exercise_day2.py
@@ -0,0 +1,36 @@
+name = "Esthe"
+age = 25
+height = 1.75
+is_learning = True
+language = "Python"
+favorite_color = 'Green'
+
+print('Name: ', name)
+print('Age: ', age)
+print('Height: ', height)
+print('Learning: ', is_learning)
+print('Language: ', language)
+print('Favorie color: ', favorite_color)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_learning))
+print(type(language))
+print(type(favorite_color))
+
+print('Name is string: ', isinstance(name, str))
+print('Age is integer: ', isinstance(age, int))
+print('Height is float: ', isinstance(height, float))
+print('Learning is boolean: ', isinstance(is_learning, bool))
+print('Favorite color is string: ', isinstance(favorite_color, str))
+
+print("First character: ", language[0])
+print('Last character: ', language[-1])
+print('Language length: ', len(language))
+print('Is Python present: ', 'Python' in language)
+print('Color length: ', len(favorite_color))
+print('Color length: ', len(favorite_color))
+print('Color length: ', len(favorite_color))
+
+
